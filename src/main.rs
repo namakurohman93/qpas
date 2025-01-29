96334e13293d9e1a731063eff2899a80cf38699a
didadadida93 <didadadida93@gmail.com>
2025-01-29 09:27:40 +0700

    Simplify process

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use url::Url;
 use clap::Parser;
 
 // TODO:
@@ -7,13 +6,12 @@ use clap::Parser;
 
 #[derive(Parser)]
 struct Cli {
-    input_url: String,
+    input_url: url::Url,
 }
 
 fn main() {
     let cli = Cli::parse();
-    let url = Url::try_from(cli.input_url.as_str()).unwrap();
-    if let Some(query) = url.query() {
+    if let Some(query) = cli.input_url.query() {
         let my_query: HashMap<String, String> = serde_qs::from_str(query).unwrap();
         let serialized = serde_json::to_string_pretty(&my_query).unwrap();
         println!("{serialized}");
