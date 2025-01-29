e186b6372699286bffd74c8f1e617d2e5c8450fb
didadadida93 <didadadida93@gmail.com>
2025-01-29 08:51:24 +0700

    Support argument input
    Add support to read input from argument
    and print output with ident.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use std::collections::HashMap;
 use url::Url;
+use clap::Parser;
 
 // TODO:
-// - get url from argument
-// - print result in JSON format (space 2 / 4?)
 // - add support to output into file when -o / --output arugment specified
 
+#[derive(Parser)]
+struct Cli {
+    input_url: String,
+}
+
 fn main() {
-    let url = Url::try_from("https://google.com?foo=bar&baz=320").unwrap();
+    let cli = Cli::parse();
+    let url = Url::try_from(cli.input_url.as_str()).unwrap();
     if let Some(query) = url.query() {
         let my_query: HashMap<String, String> = serde_qs::from_str(query).unwrap();
-        println!("{my_query:?}");
+        let serialized = serde_json::to_string_pretty(&my_query).unwrap();
+        println!("{serialized}");
     }
     else {
         println!("No query found");
